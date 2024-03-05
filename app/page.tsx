f2c750bc48c5d3c0f612eb347e1747d81b463683
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2024-03-05 14:46:22 +0500

    add contact btn

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       easing: "ease-out-cubic",
       offset: 50,
       delay: 300,
-      duration: 400
+      duration: 800
     });
   }, []);
   
@@ -84,7 +84,7 @@ export default function Home() {
             </div>
           </div> 
           <div className="order-first md:order-none relative rounded-xl overflow-hidden py-48 lg:py-56 xl:py-80">
-            <div data-aos="zoom-in" className="absolute left-0 w-full top-0 h-full">
+            <div data-aos="fade-up" className="absolute left-0 w-full top-0 h-full">
               <Image fill className="size-full object-contain" src="/assets/images/phone-2.jpg" alt='' />
             </div>
           </div>
@@ -251,6 +251,12 @@ export default function Home() {
             </div>  
           </div>
         </div>
+      </section>
+      {/* contact btn */}
+      <section className="flex items-center justify-center py-10">
+        <Link href="/contact" className="bg-brand-btn hover:bg-brand-btn-hover font-bold text-center text-white uppercase rounded-full overflow-hidden transition duration-300 px-8 xl:px-14 py-3">
+          <span>Contact us</span>
+        </Link> 
       </section>
     </main>
   );
